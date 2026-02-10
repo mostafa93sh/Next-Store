442b72277cb27d4514fa9528d92d6134ff0f0d31
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-02-10 14:05:40 +0200

    fix: add border to navbar for improved visual separation

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -7,8 +7,8 @@ import NavSearch from "./NavSearch";
 
 function Navbar() {
   return (
-    <nav>
-      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 py-8">
+    <nav className="border-b">
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 py-8 ">
         <Logo />
         <NavSearch />
         <div className="flex gap-4 items-center">
