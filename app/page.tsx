278a10d580ac677c4a6127fd1a483d949b77b676
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-02-14 20:46:44 +0200

    feat: implement HomePage layout with Hero and FeaturedProducts components, add LoadingContainer for loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from "@/components/ui/button";
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import Hero from "@/components/home/Hero";
+import { Suspense } from "react";
+
+import LoadingContainer from "@/components/globals/LoadingContainer";
 
 function HomePage() {
   return (
-    <div>
-      HomePage
-      <Button>Go to</Button>
-    </div>
+    <>
+      <Hero />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
+    </>
   );
 }
 export default HomePage;
