7247a43627c7b8b51169de8da1ec5a12a9898103
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-02-14 21:57:39 +0200

    feat: implement ProductsPage component to utilize ProductsContainer with search and layout parameters

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,11 @@
-function ProductsPage() {
-  return <div>ProductsPage</div>;
+import ProductsContainer from "@/components/products/ProductsContainer";
+type SearchParams = { layout?: string; search?: string };
+async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
+  const { layout = "grid", search = "" } = await searchParams;
+  return <ProductsContainer layout={layout} search={search} />;
 }
 export default ProductsPage;
