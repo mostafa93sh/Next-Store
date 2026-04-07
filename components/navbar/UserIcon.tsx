42c02b534c99c98a8569493bfa2d510a0e9f20f7
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-07 14:37:35 +0200

    feat(navbar): implement UserIcon component to display user profile image

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,18 @@
-function UserIcon() {
-  return <div>UserIcon</div>;
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { LuUser } from "react-icons/lu";
+async function UserIcon() {
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+  if (profileImage) {
+    return (
+      <Image
+        src={profileImage}
+        alt="profile-image"
+        className="w-6 h-6 rounded-full object-cover"
+      />
+    );
+  }
+  return <LuUser className="w-6 h-6 rounded-full bg-primary text-white" />;
 }
 export default UserIcon;
