d4b72099882199df642d93d301398d266fbe3b0b
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-07 14:37:20 +0200

    feat(navbar): implement SignOutLink with logout toast notification

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,4 +1,25 @@
+import { toast } from "sonner";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  // const toast = useToast()
+
+  const handleLogout = () => {
+    toast("Signed out successfully", {
+      description: "You have been signed out.",
+      style: {
+        backgroundColor: "whitesmoke",
+      },
+    });
+  };
+
+  return (
+    <SignOutButton>
+      <Link href={`/`} onClick={handleLogout} className="text-left w-full">
+        Sign Out
+      </Link>
+    </SignOutButton>
+  );
 }
 export default SignOutLink;
